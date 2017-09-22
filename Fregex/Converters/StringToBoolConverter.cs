9b2466f8d3cd3ef855ec0effb994b1fa50df7dcc
e200 <eleandro@inbox.ru>
2017-09-22 20:20:49 +0100

    Added StringToBoolConverter

---
+++ Fregex/Converters/StringToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Fregex.Converters
+{
+    /// <summary>
+    /// Converts string to bool
+    /// </summary>
+    class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrEmpty((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
