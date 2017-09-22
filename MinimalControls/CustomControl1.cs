c36deb397e1ce20f6c7d751e9721ee2e0a49e60b
e200 <eleandro@inbox.ru>
2017-09-22 20:22:37 +0100

    Added new solution MinimalControls

---
+++ MinimalControls/CustomControl1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MinimalControls
+{
+    /// <summary>
+    /// Siga os passos 1a ou 1b e depois 2 para usar esse controle personalizado em um arquivo XAML.
+    ///
+    /// Passo 1a) Usando o controle personalizado em um arquivo XAML que já existe no projeto atual.
+    /// Adicione o atributo XmlNamespace ao elemento raiz do arquivo de marcação onde ele 
+    /// deve ser usado:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:MinimalControls"
+    ///
+    ///
+    /// Passo 1b) Usando o controle personalizado em um arquivo XAML que existe em um projeto diferente.
+    /// Adicione o atributo XmlNamespace ao elemento raiz do arquivo de marcação onde ele 
+    /// deve ser usado:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:MinimalControls;assembly=MinimalControls"
+    ///
+    /// Também será necessário adicionar nesse projeto uma referência ao projeto que contém esse arquivo XAML
+    /// e Recompilar para evitar erros de compilação:
+    ///
+    ///     No Gerenciador de Soluções, clique com o botão direito no projeto alvo e
+    ///     "Adicionar Referência"->"Projetos"->[Selecione esse projeto]
+    ///
+    ///
+    /// Passo 2)
+    /// Vá em frente e use seu controle no arquivo XAML.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class CustomControl1 : Control
+    {
+        static CustomControl1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
+        }
+    }
+}
