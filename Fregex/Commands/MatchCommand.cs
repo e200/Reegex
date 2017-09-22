307650f08a8e8769bf73679a11e1738876ee3eb6
e200 <eleandro@inbox.ru>
2017-09-22 20:19:57 +0100

    Added MatchCommand command

---
+++ Fregex/Commands/MatchCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Fregex.Commands
+{
+    class MatchCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged = (sender, o) => { };
+
+        #region Actions
+
+        Action MatchAction;
+
+        #endregion
+
+        public MatchCommand(Action action)
+        {
+            MatchAction = action;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            MatchAction();
+        }
+    }
+}
